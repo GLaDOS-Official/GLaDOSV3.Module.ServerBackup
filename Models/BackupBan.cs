3960232c00d39b76b7140a38cb4a5009c9b20df0
BlackOfWorld <admin@blackofworld.fun>
2021-06-18 16:56:13 +0200

    first commit

---
+++ Models/BackupBan.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Rest;
+
+namespace GLaDOSV3.Module.ServerBackup.Models
+{
+    internal class BackupBan
+    {
+        public ulong Id { get; set; }
+        public string Reason { get; set; }
+        public BackupBan(RestBan b)
+        {
+            if (b == null) return;
+            Id = b.User.Id;
+            Reason = b.Reason;
+        }
+    }
+}
