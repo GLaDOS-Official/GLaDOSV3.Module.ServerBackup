70763b9e780444037968f772b3db667dbcfe4873
BlackOfWorld <admin@blackofworld.fun>
2021-08-26 03:30:35 +0200

    Fix horrible shit aka ModuleInfo

--- ModuleInfo.cs
+++ ModuleInfo.cs
@@ -4,47 +4,17 @@ using GLaDOSV3.Helpers;
 using System;
 using System.Reflection;
 using System.Runtime.Loader;
-using GLaDOSV3.Models.Interfaces;
+using GLaDOSV3.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GLaDOSV3.Module.ServerBackup
 {
-    public class ModuleInfo : IGladosModule
+    public class ModuleInfo : GladosModule
     {
-        public string Name() => "ServerBackup";
+        public override string Name => "ServerBackup";
 
-        public string Version() => "0.0.0.1";
-
-        public Uri UpdateUrl() => null;
-
-        public string Author() => "BlackOfWorld#8125";
-
-        private static volatile ModuleInfo _singleton;
-        public static IGladosModule GetModule()
-        {
-            if (_singleton != null) return _singleton;
-            _singleton = new ModuleInfo();
-            Assembly currentAssembly = Assembly.GetExecutingAssembly();
-            AssemblyLoadContext currentContext = AssemblyLoadContext.GetLoadContext(currentAssembly);
-            currentContext.Unloading += OnPluginUnloadingRequested;
-            return _singleton;
-        }
-
-        public void PreLoad(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceProvider provider)
-        { }
-
-        public void PostLoad(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceProvider provider)
-        { }
-
-        public void Reload(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceProvider provider)
-        { }
-
-        public void Unload(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceProvider provider)
-        { }
-
-        public static void OnPluginUnloadingRequested(AssemblyLoadContext obj)
-        { }
-
-        public Type[] Services(DiscordShardedClient discord, CommandService commands, BotSettingsHelper<string> config, IServiceCollection provider) => Array.Empty<Type>();
+        public override string Version => "0.0.0.1";
+        
+        public override string Author => "BlackOfWorld#8125";
     }
 }
